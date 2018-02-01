145dcd70de081821a2ddf04c1cb4b4afc9dff96c
Raph <rafal83@users.noreply.github.com>
2018-02-01 10:16:57 +0100

    Update philips.py
    Ajout des paramètres pour l'exécution en ligne de commande

--- philips.py
+++ philips.py
@@ -18,110 +18,120 @@ requests.packages.urllib3.disable_warnings()
 
 
 def createDeviceId():
-    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
+	return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
 
 
 def create_signature(secret_key, to_sign):
-    sign = HMAC.new(secret_key, to_sign, SHA)
-    return str(b64encode(sign.hexdigest().encode()))
+	sign = HMAC.new(secret_key, to_sign, SHA)
+	return str(b64encode(sign.hexdigest().encode()))
 
 def getDeviceSpecJson(config):
-    device_spec =  { "device_name" : "heliotrope", "device_os" : "Android", "app_name" : "ApplicationName", "type" : "native" }
-    device_spec['app_id'] = config['application_id']
-    device_spec['id'] = config['device_id']
-    return device_spec
+	device_spec =  { "device_name" : "heliotrope", "device_os" : "Android", "app_name" : "ApplicationName", "type" : "native" }
+	device_spec['app_id'] = config['application_id']
+	device_spec['id'] = config['device_id']
+	return device_spec
 
 
 def pair(config):
-    config['application_id'] = "app.id"
-    config['device_id'] = createDeviceId()
-    data = { 'scope' :  [ "read", "write", "control"] }
-    data['device']  = getDeviceSpecJson(config)
-    print("Starting pairing request")
-    r = requests.post("https://" + config['address'] + ":1926/6/pair/request", json=data, verify=False)
-    response = r.json()
-    auth_Timestamp = response["timestamp"]
-    config['auth_key'] = response["auth_key"]
-    auth_Timeout = response["timeout"]
+	config['application_id'] = "app.id"
+	config['device_id'] = createDeviceId()
+	data = { 'scope' :  [ "read", "write", "control"] }
+	data['device']  = getDeviceSpecJson(config)
+	print("Starting pairing request")
+	r = requests.post("https://" + config['address'] + ":1926/6/pair/request", json=data, verify=False)
+	response = r.json()
+	auth_Timestamp = response["timestamp"]
+	config['auth_key'] = response["auth_key"]
+	auth_Timeout = response["timeout"]
 
-    pin = input("Enter onscreen passcode: ")
+	pin = input("Enter onscreen passcode: ")
 
-    auth = { "auth_AppId" : "1" }
-    auth ['pin'] = str(pin)
-    auth['auth_timestamp'] = auth_Timestamp
-    auth['auth_signature'] = create_signature(b64decode(secret_key), str(auth_Timestamp).encode() + str(pin).encode())
+	auth = { "auth_AppId" : "1" }
+	auth ['pin'] = str(pin)
+	auth['auth_timestamp'] = auth_Timestamp
+	auth['auth_signature'] = create_signature(b64decode(secret_key), str(auth_Timestamp).encode() + str(pin).encode())
 
-    grant_request = {}
-    grant_request['auth'] = auth
-    grant_request['device']  = getDeviceSpecJson(config)
+	grant_request = {}
+	grant_request['auth'] = auth
+	grant_request['device']  = getDeviceSpecJson(config)
 
-    print("Attempting to pair")
-    r = requests.post("https://" + config['address'] +":1926/6/pair/grant", json=grant_request, verify=False,auth=HTTPDigestAuth(config['device_id'], config['auth_key']))
-    print(r.json())
-    print("Username for subsequent calls is: " + config['device_id'])
-    print("Password for subsequent calls is: " + config['auth_key'])
+	print("Attempting to pair")
+	r = requests.post("https://" + config['address'] +":1926/6/pair/grant", json=grant_request, verify=False,auth=HTTPDigestAuth(config['device_id'], config['auth_key']))
+	print(r.json())
+	print("Username for subsequent calls is: " + config['device_id'])
+	print("Password for subsequent calls is: " + config['auth_key'])
 
 def get_command(config):
-    r = requests.get("https://" + config['address'] + ":1926/" + config['path'], verify=False,auth=HTTPDigestAuth(config['device_id'], config['auth_key']))
-    print(r)
-    print(r.url)
-    print(r.text)
-    print(r.json())
+	r = requests.get("https://" + config['address'] + ":1926/" + config['path'], verify=False,auth=HTTPDigestAuth(config['device_id'], config['auth_key']))
+	print(r)
+	print(config)
+	if(r.ok):
+		print(r.url)
+		print(r.text)
+		print(r.json())
 
 
 def post_command(config):
-    r = requests.post("https://" + config['address'] + ":1926/" + config['path'], json=config['body'], verify=False,auth=HTTPDigestAuth(config['device_id'], config['auth_key']))
-    print(r)
+	r = requests.post("https://" + config['address'] + ":1926/" + config['path'], json=config['body'], verify=False,auth=HTTPDigestAuth(config['device_id'], config['auth_key']))
+	print(r)
+	print(config)
+	if(r.ok):
+		print(r.url)
+		print(r.text)
+		print(r.json())
 
 
 def main():
-    config={}
-    parser = argparse.ArgumentParser(description='Control a HuaFan WifiSwitch.')
-    parser.add_argument("--host", dest='host', help="Host/address of the TV")
-    parser.add_argument("--user", dest='user', help="Username")
-    parser.add_argument("--pass", dest='password', help="Password")
-    parser.add_argument("command",  help="Command to run (pair/get_volume/get/standby)")
+	config={}
+	parser = argparse.ArgumentParser(description='Control a HuaFan WifiSwitch.')
+	parser.add_argument("--host", dest='host', help="Host/address of the TV")
+	parser.add_argument("--user", dest='user', help="Username")
+	parser.add_argument("--pass", dest='password', help="Password")
+	parser.add_argument("--path",  dest='path', help="Path like 6/channeldb/tv")
+	parser.add_argument("--key", dest='key', help="")
+	parser.add_argument("--volume", dest='volume', help="")
+	parser.add_argument("--pair",  dest='pair', help="Command to pair")
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    config['address'] = args.host
+	config['address'] = args.host
  
-    if args.command == "pair":
-        pair(config)
+	if args.pair:
+		pair(config)
 
-    config['device_id'] = args.user
-    config['auth_key'] = args.password
+	config['device_id'] = args.user
+	config['auth_key'] = args.password
 
-    if args.command == "get_volume":
-        config['path'] = "6/audio/volume"
-        get_command(config)
-
-    if args.command == "get":
-        # All working commands
-        config['path'] = "6/channeldb/tv"
-        config['path'] = "6/applications"
-        config['path'] = "6/ambilight/mode"
-        config['path'] = "6/ambilight/topology"
-        config['path'] = "6/recordings/list"
-        config['path'] = "6/powerstate"
-        config['path'] = "6/ambilight/currentconfiguration"
-        config['path'] = "6/channeldb/tv/channelLists/all"
-        config['path'] = "6/system/epgsource"
-        config['path'] = "6/system"
-        config['path'] = "6/system/storage"
-        config['path'] = "6/system/timestamp"
-        config['path'] = "6/menuitems/settings/structure"
-        config['path'] = "6/ambilight/cached"
-      
-        get_command(config)
-
-    if args.command == "standby":
-        config['path'] = "6/input/key"
-        config['body'] = { "key" : "Standby" }
-        post_command(config)
+	if args.path:
+		# All working commands
+		# config['path'] = "6/channeldb/tv"
+		# config['path'] = "6/applications"
+		# config['path'] = "6/ambilight/mode"
+		# config['path'] = "6/ambilight/topology"
+		# config['path'] = "6/recordings/list"
+		# config['path'] = "6/powerstate"
+		# config['path'] = "6/ambilight/currentconfiguration"
+		# config['path'] = "6/channeldb/tv/channelLists/all"
+		# config['path'] = "6/system/epgsource"
+		# config['path'] = "6/system"
+		# config['path'] = "6/system/storage"
+		# config['path'] = "6/system/timestamp"
+		# config['path'] = "6/menuitems/settings/structure"
+		# config['path'] = "6/ambilight/cached"
+		# config['path'] = "6/input/key"
+		# config['path'] = "6/audio/volume"
+		
+		config['path'] = args.path
+		if args.key:
+			config['body'] = { "key" : args.key }
+			post_command(config)
+		elif args.volume:
+			if args.volume == 0:
+				config['body'] = { "muted" : "true", "current": args.volume }
+			else:
+				config['body'] = { "muted" : "false", "current": args.volume }
+ 			post_command(config)
+		else:
+			get_command(config)
 
 main()
-
-
-
-
